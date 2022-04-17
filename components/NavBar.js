c045342d5b16b208b2627ea081e6ff67c2fcd1d1
Scott Thompson <scott815@gmail.com>
2022-04-17 10:45:57 -0700

    updates and move folder

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,12 +1,12 @@
 const NavBar = () => {
   return (
-    <div class="navbar bg-base-100">
-      <div class="navbar-start">
-        <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost lg:hidden">
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabindex="0" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
+              className="h-5 w-5"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -21,7 +21,7 @@ const NavBar = () => {
           </label>
           <ul
             tabindex="0"
-            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
               <a>SOLUTIONS</a>
@@ -34,10 +34,10 @@ const NavBar = () => {
             </li>
           </ul>
         </div>
-        <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
       </div>
-      <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal p-0">
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">
           <li>
             <a>SOLUTIONS</a>
           </li>
@@ -49,8 +49,8 @@ const NavBar = () => {
           </li>
         </ul>
       </div>
-      <div class="navbar-end">
-        <a class="btn">Get IMGR</a>
+      <div className="navbar-end">
+        <a className="btn">Get IMGR</a>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <div class="container mx-auto">
+    <div className="container mx-auto">
       <NavBar />
       <Footer />
     </div>
